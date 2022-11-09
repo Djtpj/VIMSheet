9e42f2583c284c51fe69b0038ea86e9ab9e8f96a
Djtpj <93952263+Djtpj@users.noreply.github.com>
2022-11-08 21:48:51 -0600

    String line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
-use std::fs;
+use std::{fs, io};
 
 struct Sheet {
     content: String,
+    lines: Vec<MapLine>
 }
 
 impl Sheet {
-    pub fn new(path: &str) -> Self {
-        Sheet { 
-            content: Sheet::read_content(path),
-        }
-    }
+    pub fn new(path: String) -> Self {
+        let content: String = read_content(path);
+        let lines: Vec<MapLine> = Vec::new();
 
-    fn read_content(path: &str) -> String {
-       fs::read_to_string(path).expect("Should have been able to read the file, but couldn't") 
+        let string_lines = parse_string_lines(&content);
+
+        Sheet { 
+            content,
+            lines,
+        }
     }
 }
 
-fn main() {
-    let sheet = Sheet::new("C:\\Users\\roark\\Downloads\\addresses.csv");
+struct MapLine {
+    mode: String,
+    trigger: String,
+    description: String,
+}
 
-    println!("{}", sheet.content);
+fn main() {
+    let input = query_path();
+
+    let sheet = Sheet::new(input);
+
+    println!("{}", &sheet.content);
+}
+
+fn query_path() -> String {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    return String::from(input.trim());
+}
+
+fn parse_string_lines(content: &String) -> Vec<String> {
+    content.lines().map(|s| s.to_string()).collect()
+}
+
+fn read_content(path: String) -> String {
+    fs::read_to_string(path).expect("Should have been able to read the file, but couldn't") 
 }
